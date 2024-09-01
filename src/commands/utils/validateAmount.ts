7606407837033f1c6f3533911e3084411cfa3e9b
jojo <jojomasala@protonmail.com>
2024-09-01 02:58:08 +0100

    pass data as props; bug fix

--- src/commands/utils/validateAmount.ts
+++ src/commands/utils/validateAmount.ts
@@ -2,58 +2,68 @@ import { CommandInteraction, EmbedBuilder } from "discord.js";
 import { tryAsyncAwait } from "../../utils/tryAsyncAwait";
 import { CURRENCY_NAME_PLURAL } from "../../utils/constants";
 
+interface ValidateAmountDataProps {
+  amount: number;
+  cost: number;
+  cashBalance: number;
+}
+
+interface ValidateAmountEventProps {
+  interaction: CommandInteraction;
+  embedProps: {
+    title: string;
+    image: string;
+  };
+}
+
 export const validateAmount = async (
-  amount: number,
-  interaction: CommandInteraction,
-  cashBalance: number
-) => {
+  data: ValidateAmountDataProps,
+  event: ValidateAmountEventProps
+): Promise<boolean> => {
+  const { amount, cost, cashBalance } = data;
+  const { interaction, embedProps } = event;
+
   const embed = new EmbedBuilder()
     .setColor(0xff0000)
-    .setTitle("Coin Flip")
-    .setImage(
-      "https://media1.tenor.com/m/nk19aL-uMiUAAAAC/playing-cards-praetorian.gif"
-    )
+    .setTitle(embedProps.title)
+    .setImage(embedProps.image)
     .setAuthor({
       name: interaction.user.username,
       iconURL: interaction.user.avatarURL() || undefined,
     });
 
+  let isSuccesful = true;
   if (amount < 0) {
     embed.addFields({
       name: "Invalid amonut",
       value: `Please enter a positive amount`,
     });
-    await tryAsyncAwait(() =>
-      interaction.reply({
-        ephemeral: true,
-        embeds: [embed],
-      })
-    );
-    return;
-  } else if (amount < 50) {
+    isSuccesful = false;
+  }
+
+  if (amount < cost) {
     embed.addFields({
       name: "Invalid amonut",
-      value: `Please enter an amount greater or equal to 50`,
+      value: `Please enter an amount greater or equal to ${cost}`,
     });
-    await tryAsyncAwait(() =>
-      interaction.reply({
-        ephemeral: true,
-        embeds: [embed],
-      })
-    );
-    return;
-  } else if (amount > cashBalance) {
+    isSuccesful = false;
+  }
+
+  if (amount > cashBalance) {
     embed.addFields({
       name: `Not enough ${CURRENCY_NAME_PLURAL}`,
       value: `You do not have ${amount} ${CURRENCY_NAME_PLURAL}! Please enter a lower amount`,
     });
-    embed.setColor(0xff0000);
+    isSuccesful = false;
+  }
+
+  if (!isSuccesful)
     await tryAsyncAwait(() =>
       interaction.reply({
         ephemeral: true,
         embeds: [embed],
       })
     );
-    return;
-  }
+
+  return isSuccesful;
 };
