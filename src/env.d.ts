2bcd6bc67776172daa2a2ce6bd7fed87f0688822
jojo <jojomasala@protonmail.com>
2024-09-03 23:14:18 +0100

    new env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,10 @@ declare global {
       DISCORD_CLIENT_SECRET: string;
       DISCORD_TOKEN: string;
       UNBELIEVABOAT_TOKEN: string;
+      NOVEL_API_USERNAME: string;
+      NOVEL_API_PASSWORD: string;
+      AWS_S3_ACCESS_KEY_ID: string;
+      AWS_S3_SECRET_ACCESS_KEY: string;
     }
   }
 }
