d4d6d1cd48afdb85353f6c7477f4ec31259253bd
jojo <jojomasala@protonmail.com>
2025-04-17 01:34:40 +0100

    fix mention and wording

--- src/commands/newChannel.ts
+++ src/commands/newChannel.ts
@@ -125,7 +125,7 @@ export const NewChannel: Command = {
     );
     if (announcementChannel?.isTextBased()) {
       await announcementChannel.send({
-        content: `<@&${guildInfo.notifications.newChannelRoleId}>  <#${newChannel.id}> created thanks to <@${creditUser.user.username}>`,
+        content: `<@&${guildInfo.notifications.newChannelRoleId}> The new channel <#${newChannel.id}> has been added thanks to <@${creditUser.user.id}>`,
       });
     }
 
